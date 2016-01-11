87425a7f3413a3166037f519e15bccbe3c2ad57b
Shannon McKinney <srmckinn@gmail.com>
2016-01-10 19:11:34 -0500

    Improvements to JS show and hide of elements
    Show/hide screenshot thumbnails, give OS icons active classes depending
    on what is selected

--- _scripts/shannon.js
+++ _scripts/shannon.js
@@ -1,21 +1,45 @@
-/** Below code shows/hides the screenshot thumbnails depending on which OS icon has been clicked. **/
-
 $(document).ready(function() {
+
+    //Function to hide the OS thumbnails
     function hideElements () {
         $('#linux').hide();
         $('#osx').hide();
         $('#windows').hide();
     }
+
+    //removes active class from OS screenshot area
+    function removeOSActiveClasses() {
+        $('.active').removeClass('active');
+    }
+
+    //Shows/hide the screenshot thumbnails depending on which OS icon has been clicked
     $('.linux').click(function() {
         hideElements();
         $('#linux').show();
+        removeOSActiveClasses();
+        $('.linux').addClass('active');
     });
     $('.osx').click(function() {
         hideElements();
         $('#osx').show();
+        removeOSActiveClasses();
+        $('.osx').addClass('active');
     });
     $('.windows').click(function() {
         hideElements();
         $('#windows').show();
+        removeOSActiveClasses();
+        $('.windows').addClass('active');
     });
+
+    //Determine which browser user is on and display screenshot thumbnails on page load accordingly.
+    if ($('html').hasClass('win')) {
+        $('.windows').click();
+    }
+    else if ($('html').hasClass('linux')) {
+        $('.linux').click();
+    }
+    else {
+        $('.mac').click();
+    }
 });
