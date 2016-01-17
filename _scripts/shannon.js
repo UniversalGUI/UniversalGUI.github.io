4b429ce21f500785b3b7132d6a3571b39c728a84
Shannon McKinney <srmckinn@gmail.com>
2016-01-17 16:14:11 -0500

    Show larger screenshots and accompanying text when clicking thumbnails

--- _scripts/shannon.js
+++ _scripts/shannon.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
     //Function to hide the OS thumbnails
-    function hideElements () {
+    function hideIcons () {
         $('#linux').hide();
         $('#osx').hide();
         $('#windows').hide();
+    }
+
+    function hideScreenshots () {
+        $('.screenshots img, .screenshots h3, .screenshots p').hide();
     }
 
     //removes active class from OS screenshot area
@@ -15,32 +19,50 @@ $(document).ready(function() {
 
     //Shows/hide the screenshot thumbnails depending on which OS icon has been clicked
     $('.linux').click(function() {          //when Linux icon is clicked
-        hideElements();                     //call the function that hides all of the OS elements
+        hideIcons();                     //call the function that hides all of the OS elements
         $('#linux').show();                 //show the Linux screenshot thumbnails
         removeOSActiveClasses();            //remove the Active class from all of the OS icons (takes away orange highlight from everything)
         $('.linux').addClass('active');     //add class "active" to Linux so that that one becomes highlighted
     });
     $('.osx').click(function() {
-        hideElements();
+        hideIcons();
         $('#osx').show();
         removeOSActiveClasses();
         $('.osx').addClass('active');
     });
     $('.windows').click(function() {
-        hideElements();
+        hideIcons();
         $('#windows').show();
         removeOSActiveClasses();
         $('.windows').addClass('active');
     });
 
-    //Determine which browser user is on and display screenshot thumbnails on page load accordingly.
+    $('#win-thumb').click(function() {
+        hideScreenshots();
+        $('.win-screenshot-lg').show();
+    });
+
+    $('#osx-thumb').click(function() {
+        hideScreenshots();
+        $('.osx-screenshot-lg').show();
+    });
+
+    $('#lin-thumb').click(function() {
+        hideScreenshots();
+        $('.linux-screenshot-lg').show();
+    });
+
+    //Determine which browser user is on and display screenshot thumbnails & default screenshot on page load accordingly.
     if ($('html').hasClass('win')) {
         $('.windows').click();
+        $('#win-thumb').click();
     }
     else if ($('html').hasClass('linux')) {
         $('.linux').click();
+        $('#lin-thumb').click();
     }
     else {
         $('.mac').click();
+        $('#osx-thumb').click();
     }
 });
