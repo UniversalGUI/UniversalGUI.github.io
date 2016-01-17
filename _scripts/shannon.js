4b6b85d4a009b409ddef5f326ab148d41164ab21
Shannon McKinney <srmckinn@gmail.com>
2016-01-17 12:59:07 -0500

    Minor changes
    Updated margin for .screenshots div, commented jQuery pseudo-code

--- _scripts/shannon.js
+++ _scripts/shannon.js
@@ -8,16 +8,17 @@ $(document).ready(function() {
     }
 
     //removes active class from OS screenshot area
+    //the active class in this area makes the background color change to orange and gives it a grayish right border
     function removeOSActiveClasses() {
         $('.active').removeClass('active');
     }
 
     //Shows/hide the screenshot thumbnails depending on which OS icon has been clicked
-    $('.linux').click(function() {
-        hideElements();
-        $('#linux').show();
-        removeOSActiveClasses();
-        $('.linux').addClass('active');
+    $('.linux').click(function() {          //when Linux icon is clicked
+        hideElements();                     //call the function that hides all of the OS elements
+        $('#linux').show();                 //show the Linux screenshot thumbnails
+        removeOSActiveClasses();            //remove the Active class from all of the OS icons (takes away orange highlight from everything)
+        $('.linux').addClass('active');     //add class "active" to Linux so that that one becomes highlighted
     });
     $('.osx').click(function() {
         hideElements();
