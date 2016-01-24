fdfa8ddeffada16a436496c2caa5f9923823a854
Shannon McKinney <srmckinn@gmail.com>
2016-01-24 18:40:11 -0500

    Modified JS on homepage to work from array of objects
    Modified the JS, homepage, and CSS to more dynamically load content in
    the Screenshots section of the page.

--- _scripts/shannon.js
+++ _scripts/shannon.js
@@ -1,68 +1,83 @@
 $(document).ready(function() {
 
-    //Function to hide the OS thumbnails
-    function hideIcons () {
-        $('#linux').hide();
-        $('#osx').hide();
-        $('#windows').hide();
+    var screenshotData = [
+        {
+            "os": "win",
+            "large": "ugui-win.png",
+            "largalt": "Screenshot of UGUI running on Windows",
+            "thumb": "ugui-win-th.png",
+            "thumbalt": "Thumbnail of a screenshot of UGUI running on Windows",
+            "title": "UGUI Running on Windows",
+            "paragraph": ""
+        },
+        {
+            "os": "osx",
+            "thumb": "ugui-osx-th.png",
+            "large": "ugui-osx.png",
+            "thumbalt": "Thumbnail of a screenshot of UGUI running on OSX",
+            "largealt": "Screenshot of UGUI running on OSX",
+            "title": "UGUI Running on OSX",
+            "paragraph":  ""
+        },
+        {
+            "os": "ubuntu",
+            "thumb": "ugui-ubuntu-th.png",
+            "large": "_img/screenshot/ugui-ubuntu.png",
+            "thumbalt": "Thumbnail of a screenshot of UGUI running on Ubuntu",
+            "largealt": "Screenshot of UGUI running on Ubuntu",
+            "title": "UGUI Running on Ubuntu",
+            "paragraph": ""
+        }
+    ];
+
+    for (var i = 0; i < screenshotData.length; i++) {
+        var os = screenshotData[i].os;
+        var thumbImage = screenshotData[i].thumb;
+        var thumbAltText = screenshotData[i].thumbalt;
+        $(".thumbnails").append(
+            '<img class="' + os + '" src="_img/screenshot/' + thumbImage + '" alt="' + thumbAltText + '" />'
+        );
     }
 
-    function hideScreenshots () {
-        $('.screenshots img, .screenshots h3, .screenshots p').hide();
-    }
-
-    //removes active class from OS screenshot area
-    //the active class in this area makes the background color change to orange and gives it a grayish right border
-    function removeOSActiveClasses() {
-        $('.active').removeClass('active');
-    }
-
-    //Shows/hide the screenshot thumbnails depending on which OS icon has been clicked
-    $('.linux').click(function() {          //when Linux icon is clicked
-        hideIcons();                        //call the function that hides all of the OS elements
-        $('#linux').show();                 //show the Linux screenshot thumbnails
-        removeOSActiveClasses();            //remove the Active class from all of the OS icons (takes away orange highlight from everything)
-        $('.linux').addClass('active');     //add class "active" to Linux so that that one becomes highlighted
-    });
-    $('.osx').click(function() {
-        hideIcons();
-        $('#osx').show();
-        removeOSActiveClasses();
-        $('.osx').addClass('active');
-    });
-    $('.windows').click(function() {
-        hideIcons();
-        $('#windows').show();
-        removeOSActiveClasses();
-        $('.windows').addClass('active');
+    $("#screenshots header div").click(function() {
+        $("#screenshots header div").removeClass("active");
+        var os = $(this).attr("class");
+        $(this).addClass("active");
+        $(".thumbnails img").hide();
+        $(".thumbnails ." + os).show();
     });
 
-    $('#win-thumb').click(function() {
-        hideScreenshots();
-        $('.win-screenshot-lg').show();
-    });
-
-    $('#osx-thumb').click(function() {
-        hideScreenshots();
-        $('.osx-screenshot-lg').show();
-    });
-
-    $('#lin-thumb').click(function() {
-        hideScreenshots();
-        $('.linux-screenshot-lg').show();
-    });
 
     //Determine which browser user is on and display screenshot thumbnails & default screenshot on page load accordingly.
     if ($('html').hasClass('win')) {
-        $('.windows').click();
-        $('#win-thumb').click();
+        $('#screenshots header .win').click();
+        for (var i = 0; i < screenshotData.length; i++) {
+            var os = screenshotData[i].os;
+            if (os == "win") {
+                $( $(".thumbnails img")[i] ).click();
+                return;
+            }
+        }
     }
     else if ($('html').hasClass('linux')) {
-        $('.linux').click();
-        $('#lin-thumb').click();
+        $('#screenshots header .ubuntu').click();
+        for (var i = 0; i < screenshotData.length; i++) {
+            var os = screenshotData[i].os;
+            if (os == "ubuntu") {
+                $( $(".thumbnails img")[i] ).click();
+                return;
+            }
+        }
     }
     else {
-        $('.osx').click();
-        $('#osx-thumb').click();
+        $('#screenshots header .osx').click();
+        for (var i = 0; i < screenshotData.length; i++) {
+            var os = screenshotData[i].os;
+            if (os == "osx") {
+                $( $(".thumbnails img")[i] ).click();
+                return;
+            }
+        }
     }
+
 });
